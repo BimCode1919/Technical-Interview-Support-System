aaef0ce45e9b70e53e29b621964c0646bc91043c
Tran Quy <gforwork1803@gmail.com>
2026-01-12 07:31:51 +0700

    First commit

---
+++ tiss-back-end/src/main/java/org/tiss/tissbackend/TissBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.tiss.tissbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TissBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TissBackEndApplication.class, args);
+    }
+
+}
---
+++ tiss-back-end/src/test/java/org/tiss/tissbackend/TissBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tiss.tissbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TissBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
